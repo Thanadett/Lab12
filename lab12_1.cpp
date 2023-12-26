3e3da139a85cf1dfffba6654b5647bbe0402d48c
Thanadett <tanapremin@gmail.com>
2023-12-26 15:04:31 +0700

    lab12_1.cpp completed V.2

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int fibonacci(int x);
+long long int fibonacci(long long int x);
 
 int main()
 {
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
     return 0;
 }
 
-int fibonacci(int x)
+long long int fibonacci(long long int x)
 {
     if (x == 0)
     {
